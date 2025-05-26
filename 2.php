1780dddaa267028c0febd3ceffbabeb7204a956d
alaa-naeem <alaa_naeem@hotmail.com>
2025-05-26 21:09:11 +0300

    commit all

--- 2.php
+++ 2.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>third file</title>
+    <title>php page</title>
 </head>
-
 <body>
-<ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-</ul>
+    
 </body>
-
 </html>
